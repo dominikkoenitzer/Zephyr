5d886605c72603fde88d8d1157db0253fda6377e
Dominik Könitzer <dominik.koenitzer@gmail.com>
2025-01-15 12:58:24 +0100

    Delete Breeze-Flow-Backend directory

--- Breeze-Flow-Backend/src/main/java/com/breeze_flow/breeze_flow/repository/TaskRepository.java
+++
@@ -1,8 +0,0 @@
-package com.breeze_flow.breeze_flow.repository;
-
-import com.breeze_flow.breeze_flow.model.Task;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface TaskRepository extends MongoRepository<Task, String> {
-    // Add custom queries if needed
-} 
