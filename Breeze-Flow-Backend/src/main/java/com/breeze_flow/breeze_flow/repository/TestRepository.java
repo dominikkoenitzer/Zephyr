4ac4a620e42d8273998eb37c1dbaaa06e280b9dd
dominikkoenitzer <dominikkoenitzer@users.noreply.github.com>
2025-01-15 12:58:24 +0100

    Delete Breeze-Flow-Backend directory

--- Breeze-Flow-Backend/src/main/java/com/breeze_flow/breeze_flow/repository/TestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.breeze_flow.breeze_flow.repository;
-
-import com.breeze_flow.breeze_flow.model.TestModel;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface TestRepository extends MongoRepository<TestModel, String> {
-} 
