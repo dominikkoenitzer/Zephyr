b2cfe2bb46a26eabb03cae8bc9e6aa3f066baba6
Dominik Könitzer <dominik.koenitzer@gmail.com>
2025-11-15 20:58:39 +0100

    Add borders to all pages for better visual structure

--- src/pages/Journal.jsx
+++ src/pages/Journal.jsx
@@ -2,7 +2,7 @@ import Journal from '../components/Journal/Journal';
 
 function JournalPage() {
   return (
-    <div className="w-full">
+    <div className="w-full border border-border rounded-lg p-4 sm:p-6 bg-card">
       <Journal />
     </div>
   );
