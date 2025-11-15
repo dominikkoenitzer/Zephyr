7efa4c32df2593ba54af4df1e78dc148e1f44b61
dominikkoenitzer <dominik.koenitzer@gmail.com>
2025-11-15 21:06:14 +0100

    Fix content overflow at bottom - use max-height and min-h-0 for proper flexbox constraints

--- src/pages/Journal.jsx
+++ src/pages/Journal.jsx
@@ -2,7 +2,7 @@ import Journal from '../components/Journal/Journal';
 
 function JournalPage() {
   return (
-    <div className="w-full h-full lg:h-[calc(100vh-4rem)] border-2 border-border rounded-xl p-2 sm:p-3 md:p-4 lg:p-5 xl:p-6 bg-card lg:overflow-hidden overflow-y-auto lg:overflow-y-hidden flex flex-col">
+    <div className="w-full h-full lg:max-h-full lg:h-full border-2 border-border rounded-xl p-2 sm:p-3 md:p-4 lg:p-5 xl:p-6 bg-card lg:overflow-hidden overflow-y-auto lg:overflow-y-hidden flex flex-col min-h-0">
       <Journal />
     </div>
   );
