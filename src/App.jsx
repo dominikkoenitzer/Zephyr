bb1871631f3062f2e340ec218588d9626a2719b6
Dominik Könitzer <dominik.koenitzer@gmail.com>
2025-11-15 21:06:14 +0100

    Fix content overflow at bottom - use max-height and min-h-0 for proper flexbox constraints

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,7 @@ function App() {
       <div className="lg:pl-72 h-full lg:h-screen flex flex-col flex-1 lg:overflow-hidden">
         <Header onMenuClick={handleSidebarOpen} />
         <main className="flex-1 lg:overflow-hidden lg:flex lg:items-center lg:justify-center overflow-y-auto lg:overflow-y-hidden pt-16 lg:pt-16">
-          <div className="w-full h-full lg:h-[calc(100vh-4rem)] px-2 sm:px-3 md:px-4 lg:px-6 xl:px-8 2xl:px-10 py-2 sm:py-3 md:py-4 lg:py-5 xl:py-6">
+          <div className="w-full h-full lg:max-h-[calc(100vh-4rem)] lg:h-[calc(100vh-4rem)] px-2 sm:px-3 md:px-4 lg:px-6 xl:px-8 2xl:px-10 py-2 sm:py-3 md:py-4 lg:py-5 xl:py-6 flex flex-col">
             <Outlet />
           </div>
         </main>
